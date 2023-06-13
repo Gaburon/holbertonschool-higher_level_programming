108872889b1ad0c5f0a2cb4eaaf0bdd4bc42625a
gaburon <japo000@hotmail.com>
2023-06-13 16:28:18 -0500

    Si

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
         """initialize size"""
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size <= 0:
+        elif size <= 0:
             raise TypeError("size must be >= 0")
         else:
             self.__size = size
