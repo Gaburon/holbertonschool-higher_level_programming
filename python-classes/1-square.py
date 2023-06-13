1d41699cbb3757b1831b67ec0a8d6745ddb5168b
gaburon <japo000@hotmail.com>
2023-06-13 15:17:47 -0500

    First commit

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class square"""
+
+
+class square:
+    """Defines a square"""
+    def __init__(self, size):
+        """initialize size"""
+        self.size = size
