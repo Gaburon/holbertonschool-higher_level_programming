18dc0ce37e2e8a851de7b6a8830034ec7a291b22
gaburon <japo000@hotmail.com>
2023-06-15 13:19:01 -0500

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-""" Add integer
-    This modlue adds two numbers"""
+""" 0-add_integer Module """
 
 
 def add_integer(a, b=98):
-    """ Argumens:
-        a: first number to add
-        b: second number to add
-
-        Returns:
-            int: the sum of bot numbers casted to integers
-            Typerror if a or b is not a number
-    """
-
-    if isinstance(a, int) is False and isinstance(a, float) is False:
+    """ Addition Function for Integers """
+    if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if isinstance(b, int) is False and isinstance(b, float) is False:
+    if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
     return int(a) + int(b)
