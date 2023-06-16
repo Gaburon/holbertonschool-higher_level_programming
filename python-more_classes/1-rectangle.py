b226fb3945f705ff199cbea3c8b3356da868c254
gaburon <japo000@hotmail.com>
2023-06-16 14:22:53 -0500

    1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -17,14 +17,14 @@ class Rectangle:
         """
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """
         Setting the width of the new rectangle.
         """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) not in [int]:
@@ -32,14 +32,14 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """
         Setting the height of the new rectangle.
         """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if type(value) not in [int]:
