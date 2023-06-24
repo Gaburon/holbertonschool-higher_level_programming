367f95ee36c6b9eb14c8143aa24ef27abf31e80b
gaburon <japo000@hotmail.com>
2023-06-24 17:16:29 -0500

    Fix attemp

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,51 @@
 #!/usr/bin/python3
-""" script that adds all arguments to a Python list,
-and then save them to a file
+
+""" This module writes a script that adds all arguments to a Python list,
+and then save them to a file.
+You must use your function save_to_json_file from 5-save_to_json_file.py.
+You must use your function load_from_json_file from 6-load_from_json_file.py.
+The list must be saved as a JSON representation in a file named add_item.json.
+If the file doesn’t exist, it should be created
+Does not manage file permissions / exceptions.
 """
 
 
-from sys import argv
+import sys
+from typing import List
+import importlib
 
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+# Use importlib to import modules with non-standard names
+save_to_json_file = (
+    importlib.import_module('5-save_to_json_file').save_to_json_file
+)
+load_from_json_file = (
+    importlib.import_module('6-load_from_json_file').load_from_json_file
+)
 
-fname = "add_item.json"
 
-try:
-    content = load_from_json_file(fname)
-except FileNotFoundError:
-    content = []
+def add_items_to_list(args: List[str]):
+    """ script that adds all arguments to a Python list,
+and then save them to a file.
 
-save_to_json_file(content + argv[1:], fname)
+    Args:
+        args (List[str]): args are key to a list of strings
+    """
+    try:
+        # Load existing items from file, if it exists
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        # If file doesn't exist, start with an empty list
+        items = []
+
+    # Add new items to the list
+    items.extend(args)
+
+    # Save the updated list to file
+    save_to_json_file(items, "add_item.json")
+
+
+if __name__ == "__main__":
+    # Remove the script name from arguments
+    arguments = sys.argv[1:]
+    add_items_to_list(arguments)
+    
