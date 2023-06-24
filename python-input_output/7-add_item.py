8cd627eb7b182c20374990616be2b22402468633
gaburon <japo000@hotmail.com>
2023-06-24 16:52:01 -0500

    Si

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Shawn is the only child now"""
+""" script that adds all arguments to a Python list,
+and then save them to a file"""
 
 
-import sys
+from sys import argv
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
+fname = "add_item.json"
 
-    try:
-        items = load_from_json_file('add_item.json')
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, 'add_item.json')
+try:
+    content = load_from_json_file(fname)
+except FileNotFoundError:
+    content = []
+
+save_to_json_file(content + argv[1:], fname)
