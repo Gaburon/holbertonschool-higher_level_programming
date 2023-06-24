c93946139f107ea9cdd09cbb5aa1f8fc832e057a
gaburon <japo000@hotmail.com>
2023-06-24 16:40:46 -0500

    Fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 """
-Importing.
+    Script to save arguments to a file
 """
-import sys
 
+import sys
+import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    try:
-        validation = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        validation = []
-    validation.extend(sys.argv[1:])
-    save_to_json_file(validation, "add_item.json")
+try:
+    arguments = load_from_json_file("add_item.json")
+except Exception:
+    arguments = []
+
+i = 1
+while i < len(sys.argv):
+    arguments.append(sys.argv[i])
+    i += 1
+
+with open("add_item.json", encoding='utf-8', mode='w') as file:
+    file.write(json.dumps(arguments))
