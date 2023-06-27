9cd98ddf008e3485ac9810b2bc4d60c9f8495274
gaburon <japo000@hotmail.com>
2023-06-27 16:56:36 -0500

    Sisa

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module 1-Base_Class
+"""
+
+
+class Base:
+    """
+    class that will be de base of te other classes
+    """
+    def __init__(self, id=None):
+        """
+        Constructor
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base._nb_objects += 1
+            self.id = Base._nb_objects
