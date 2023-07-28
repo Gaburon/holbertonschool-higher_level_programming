5dd23b1cf63b004519308253e756891598fbdc80
gaburon <japo000@hotmail.com>
2023-07-28 15:09:53 -0500

    sisa

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''
+It list all states from database hbtn_0e_0_usa
+'''
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+
+    cur.close()
